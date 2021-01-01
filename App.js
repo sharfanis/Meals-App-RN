a4c6505922ac7d31afb6b88cfc3b63bfd6a531d2
Shabih <Shabih.sharfani@gmail.com>
2021-01-01 15:26:04 -0600

    chore(): Test2

--- App.js
+++ App.js
@@ -5,10 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! yay!yyy</Text>
-
-
-      
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
